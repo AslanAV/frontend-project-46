f66b9390c01bd808ee041d4e88c9d8c628d2a7db
AslanAV <autlevaslan@yandex.ru>
2023-08-18 00:32:33 +0300

    hexlet check fix

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -3,16 +3,14 @@ import plainFormatter from './plain.js';
 import jsonFormatter from './json.js';
 
 const getFormatDiff = (diff, format) => {
-  if (format === 'stylish') {
-    return stylishFormatter(diff);
-  }
   if (format === 'plain') {
     return plainFormatter(diff);
   }
   if (format === 'json') {
     return jsonFormatter(diff);
   }
-  throw new Error('Format is incorrect!');
+
+  return stylishFormatter(diff);
 };
 
 export default getFormatDiff;
